07727f2dbc61f798efec683c044d28aaa4857d17
Stephen Mather <stephen@smathermather.com>
2015-06-02 09:27:34 -0400

    featureLayer <> featuresLayer

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -48,7 +48,7 @@ featureLayer.eachLayer(function(layer) {
     layer.bindPopup(content);
 });
 
-var searchControl = new L.Control.Search({layer: featuresLayer, propertyName: 'locationb', circleLocation:false});
+var searchControl = new L.Control.Search({layer: featureLayer, propertyName: 'locationb', circleLocation:false});
 
 	searchControl.on('search_locationfound', function(e) {
 		
